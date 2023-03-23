d92f460fd53de32a33fa2ea1ad5c33ada65b35e7
cafe51 <cafecafe51@hotmail.com>
2023-03-23 13:41:24 -0400

    requisito 3 resolvido

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,22 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate_company_product_list(list_of_dicts):
+        companies = Counter(
+            [dict["nome_da_empresa"] for dict in list_of_dicts]
+        )
+        report = [
+            f"- {company}: {companies[company]}" for company in companies
+        ]
+        return "\n".join(report)
+
+    @staticmethod
+    def generate(list_of_dicts):
+        return (
+            f"{SimpleReport.generate(list_of_dicts)}\n"
+            f"Produtos estocados por empresa:\n"
+            f"{CompleteReport.generate_company_product_list(list_of_dicts)}\n"
+        )
