f890b971e4637e4fbabd54d83b1c8d3dce0b9b42
cafe51 <cafecafe51@hotmail.com>
2023-03-23 17:19:57 -0400

    requisito 5 resolvido

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,17 +1,28 @@
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
+import json
 
 
 class Inventory:
     @staticmethod
-    def import_data(path, type):
-        list_of_dicts = []
-        with open(path) as file:
-            list_of_dicts = [dict(row) for row in csv.DictReader(file)]
+    def generate(list_of_dicts, type):
         if type == "simples":
             return SimpleReport.generate(list_of_dicts)
         elif type == "completo":
             return CompleteReport.generate(list_of_dicts)
         else:
             raise TypeError(f"{type} não pode ser aceito: VALOR ERRADO")
+
+    @staticmethod
+    def import_data(path: str, type):
+        if path.endswith(".csv"):
+            with open(path) as file:
+                list_of_dicts = [dict(row) for row in csv.DictReader(file)]
+                return Inventory.generate(list_of_dicts, type)
+        elif path.endswith(".json"):
+            with open(path) as file:
+                list_of_dicts = [dict(row) for row in json.load(file)]
+                return Inventory.generate(list_of_dicts, type)
+        else:
+            raise ValueError("CAMINHO ERRADO OU FORMATO DESCONHECIDO")
