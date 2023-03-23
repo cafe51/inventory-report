dbd798d7990f9ecd9120ff24bbb10023473b9b88
cafe51 <cafecafe51@hotmail.com>
2023-03-22 22:12:57 -0400

    primeiro requisito resolvido

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,23 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+from datetime import date
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    today = date.today()
+    product = Product(
+        12,
+        'ventilador',
+        'Mondial',
+        today,
+        today,
+        '123456',
+        'em local ventilado',
+    )
+
+    assert product.id == 12
+    assert product.nome_do_produto == 'ventilador'
+    assert product.nome_da_empresa == 'Mondial'
+    assert product.data_de_fabricacao == str(today)
+    assert product.data_de_validade == str(today)
+    assert product.numero_de_serie == '123456'
+    assert product.instrucoes_de_armazenamento == 'em local ventilado'
