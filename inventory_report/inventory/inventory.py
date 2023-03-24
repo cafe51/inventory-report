b4c2621e1045fb6cdd0575a522f885790dcc7832
cafe51 <cafecafe51@hotmail.com>
2023-03-24 01:08:02 -0400

    inventory modificado

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,36 +1,16 @@
-from inventory_report.reports.simple_report import SimpleReport
-from inventory_report.reports.complete_report import CompleteReport
-import csv
-import json
-import xmltodict
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.xml_importer import XmlImporter
 
 
 class Inventory:
     @staticmethod
-    def generate(list_of_dicts, type):
-        if type == "simples":
-            return SimpleReport.generate(list_of_dicts)
-        elif type == "completo":
-            return CompleteReport.generate(list_of_dicts)
-        else:
-            raise TypeError(f"{type} não pode ser aceito: VALOR ERRADO")
-
-    @staticmethod
-    def import_data(path: str, type):
+    def import_data(path, type):
+        if path.endswith(".json"):
+            return JsonImporter.import_data(path, type)
         if path.endswith(".csv"):
-            with open(path) as file:
-                list_of_dicts = [dict(row) for row in csv.DictReader(file)]
-                return Inventory.generate(list_of_dicts, type)
-        elif path.endswith(".json"):
-            with open(path) as file:
-                list_of_dicts = [dict(row) for row in json.load(file)]
-                return Inventory.generate(list_of_dicts, type)
-        elif path.endswith(".xml"):
-            with open(path) as file:
-                list_of_dicts = xmltodict.parse(file.read())["dataset"][
-                    "record"
-                ]
-                return Inventory.generate(list_of_dicts, type)
-
+            return CsvImporter.import_data(path, type)
+        if path.endswith(".xml"):
+            return XmlImporter.import_data(path, type)
         else:
             raise ValueError("Arquivo inválido")
