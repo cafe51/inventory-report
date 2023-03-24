ed7bf616eb84cbf84d8130db000504f890756e91
cafe51 <cafecafe51@hotmail.com>
2023-03-24 01:47:24 -0400

    requisito 8 resolvido

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    output = (
+        "O produto Farinha fabricado "
+        "em 2021-02-18 por Farinini com validade até 2023-09-17 "
+        "precisa ser armazenado ao abrigo de luz."
+    )
+    farinha = Product(
+        1,
+        "Farinha",
+        "Farinini",
+        "2021-02-18",
+        "2023-09-17",
+        "CR25 1551 4467 2549 4402",
+        "ao abrigo de luz",
+    )
+
+    assert output == farinha.__repr__()
