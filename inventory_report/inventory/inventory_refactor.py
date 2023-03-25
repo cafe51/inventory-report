05b1239f8bd760b3b41fa5aede8fbddb35054f51
cafe51 <cafecafe51@hotmail.com>
2023-03-25 02:12:04 -0400

    ultimo requisito resolvido

--- inventory_report/inventory/inventory_refactor.py
+++ inventory_report/inventory/inventory_refactor.py
@@ -1,5 +1,7 @@
 from collections.abc import Iterable
 from inventory_report.inventory.inventory_iterator import InventoryIterator
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
 
 
 class InventoryRefactor(Iterable):
@@ -10,8 +12,20 @@ class InventoryRefactor(Iterable):
     def data_imported(self, path):
         self.data += self.importer.import_data(path)
 
-    def import_data(self, path, fake_parameter):
+    def simple_or_completo(self, type):
+        if type == "simples":
+            return SimpleReport.generate(self.data)
+        elif type == "completo":
+            return CompleteReport.generate(self.data)
+        else:
+            raise TypeError(f"{type} não pode ser aceito: VALOR ERRADO")
+
+    def import_data(self, path, type):
         self.data_imported(path)
+        try:
+            return self.simple_or_completo(type)
+        except ValueError:
+            raise ValueError("Arquivo inválido")
 
     def __iter__(self):
         return InventoryIterator(self.data)
--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,31 @@
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.xml_importer import XmlImporter
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+import sys
+
+
+def define_instance(path):
+    if path.endswith(".json"):
+        return InventoryRefactor(JsonImporter)
+    if path.endswith(".csv"):
+        return InventoryRefactor(CsvImporter)
+    if path.endswith(".xml"):
+        return InventoryRefactor(XmlImporter)
+    else:
+        raise ValueError("Arquivo inválido")
+
+
 def main():
-    pass
+    if len(sys.argv) < 3:
+        sys.stderr.write("Verifique os argumentos\n")
+    else:
+        _, caminho_do_arquivo_input, tipo_de_relatório, *others = sys.argv
+
+        instance = define_instance(caminho_do_arquivo_input)
+
+        response = instance.import_data(
+            caminho_do_arquivo_input, tipo_de_relatório
+        )
+        print(response, end="")
+
