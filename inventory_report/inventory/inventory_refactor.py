a05751af279b5a3bd61ea75f5a37103428493fc4
cafe51 <cafecafe51@hotmail.com>
2023-03-24 21:11:26 -0400

    requisito 10 finalmente resolvido

--- inventory_report/inventory/inventory_refactor.py
+++ inventory_report/inventory/inventory_refactor.py
@@ -1,37 +1,17 @@
 from collections.abc import Iterable
 from inventory_report.inventory.inventory_iterator import InventoryIterator
-from inventory_report.inventory.product import Product
 
 
 class InventoryRefactor(Iterable):
     def __init__(self, importer) -> None:
         self.importer = importer
         self.data = []
-        self.products = []
 
     def data_imported(self, path):
         self.data = self.importer.import_data(path)
-        # return self.data
 
     def import_data(self, path, fake_parameter):
         self.data_imported(path)
-        list_of_dicts = self.data
-        list_of_products = [
-            Product(
-                dictionary["id"],
-                dictionary["nome_do_produto"],
-                dictionary["nome_da_empresa"],
-                dictionary["data_de_fabricacao"],
-                dictionary["data_de_validade"],
-                dictionary["numero_de_serie"],
-                dictionary["instrucoes_de_armazenamento"],
-            ).__repr__()
-            for dictionary in list_of_dicts
-        ]
-
-        self.products = list_of_products
-
-        return list_of_products
 
     def __iter__(self):
-        return InventoryIterator(self.products)
+        return InventoryIterator(self.data)
